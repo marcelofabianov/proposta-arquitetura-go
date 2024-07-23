981319c9abe63384aec93ca02892e3fc99c582ef
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-23 18:31:58 -0300

    feat: criar erros e comportamento para erros de domain

--- modules/user/domain/error.go
+++ modules/user/domain/error.go
@@ -1,8 +1,31 @@
 package domain
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
-	ErrUserNotFound = errors.New("error_user_not_found")
-	ErrEmailExists  = errors.New("error_email_exists")
+	ErrUserPasswordHashFailed   = errors.New("error_user_password_hash_failed")
+	ErrUserPersistNewUserFailed = errors.New("error_user_persist_new_user_failed")
 )
+
+func IsErrUserPasswordHashFailed(err error) bool {
+	return errors.Is(err, ErrUserPasswordHashFailed)
+}
+
+func IsErrUserPersistNewUserFailed(err error) bool {
+	return errors.Is(err, ErrUserPersistNewUserFailed)
+}
+
+func GetErrUserPasswordHashFailed(err error) error {
+	fmt.Println("Error: ", err)
+
+	return ErrUserPasswordHashFailed
+}
+
+func GetErrUserPersistNewUserFailed(err error) error {
+	fmt.Println("Error: ", err)
+
+	return ErrUserPersistNewUserFailed
+}
