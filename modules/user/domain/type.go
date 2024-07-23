40f76a45fffa34ff34e5a1017b390c2018a7ddcd
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-23 18:31:13 -0300

    feat: criar inicializadores dos tipos de domain

--- modules/user/domain/type.go
+++ modules/user/domain/type.go
@@ -1,12 +1,32 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type ID string
 type Email string
 type Password string
-type EnabledAt time.Time
+type EnabledAt *time.Time
 type CreatedAt time.Time
 type UpdatedAt time.Time
 type DeletedAt *time.Time
 type Version int
+
+func NewID() ID {
+	return ID(uuid.New().String())
+}
+
+func NewCreatedAt() CreatedAt {
+	return CreatedAt(time.Now())
+}
+
+func NewUpdatedAt() UpdatedAt {
+	return UpdatedAt(time.Now())
+}
+
+func NewVersion() Version {
+	return Version(1)
+}
