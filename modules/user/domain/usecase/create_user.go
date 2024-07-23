d09b55eceabe18b24ab5b1feedf911e0da06513e
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-23 18:35:27 -0300

    feat: adicionar caso de uso CreateUserUseCase

--- modules/user/domain/usecase/create_user.go
+++ modules/user/domain/usecase/create_user.go
@@ -1,2 +1,42 @@
 package usecase
 
+import (
+	"context"
+
+	"example/modules/user/domain"
+	"example/modules/user/port/inbound/feature"
+)
+
+type CreateUserUseCase struct {
+	repository feature.CreateUserRepository
+	hasher     feature.PasswordHasher
+}
+
+func NewCreateUserUseCase(repository feature.CreateUserRepository, hasher feature.PasswordHasher) feature.CreateUserUseCase {
+	return &CreateUserUseCase{repository: repository, hasher: hasher}
+}
+
+func (uc *CreateUserUseCase) Execute(ctx context.Context, input feature.CreateUserUseCaseInput) (feature.CreateUserUseCaseOutput, error) {
+	hashedPassword, err := uc.hasher.Hash(input.Password)
+	if err != nil {
+		return feature.CreateUserUseCaseOutput{}, domain.GetErrUserPasswordHashFailed(err)
+	}
+
+	user := domain.User{
+		ID:        domain.NewID(),
+		Name:      input.Name,
+		Email:     domain.Email(input.Email),
+		Password:  domain.Password(hashedPassword),
+		EnabledAt: domain.EnabledAt(nil),
+		CreatedAt: domain.NewCreatedAt(),
+		UpdatedAt: domain.NewUpdatedAt(),
+		Version:   domain.NewVersion(),
+	}
+
+	err = uc.repository.CreateUser(ctx, feature.CreateUserRepositoryInput{User: user})
+	if err != nil {
+		return feature.CreateUserUseCaseOutput{}, domain.GetErrUserPersistNewUserFailed(err)
+	}
+
+	return feature.CreateUserUseCaseOutput{User: user}, nil
+}
