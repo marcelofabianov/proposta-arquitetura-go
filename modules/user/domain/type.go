138af2c7cecf5f955aba1b5717607971143880f9
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-24 06:44:00 -0300

    feat: adicionar func em type para converter tipos

--- modules/user/domain/type.go
+++ modules/user/domain/type.go
@@ -30,3 +30,27 @@ func NewUpdatedAt() UpdatedAt {
 func NewVersion() Version {
 	return Version(0)
 }
+
+func (c CreatedAt) String() string {
+	return time.Time(c).Format(time.RFC3339)
+}
+
+func (u UpdatedAt) String() string {
+	return time.Time(u).Format(time.RFC3339)
+}
+
+func (v Version) String() int64 {
+	return int64(v)
+}
+
+func (i ID) String() string {
+	return string(i)
+}
+
+func (e Email) String() string {
+	return string(e)
+}
+
+func (p Password) String() string {
+	return string(p)
+}
