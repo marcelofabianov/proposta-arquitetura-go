6f09006497bb98924b72b60ac9c2c4d13b69fe59
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-24 06:44:30 -0300

    feat: conversao de valores de input no CreateUser do UserRepository

--- modules/user/adapter/pg_repository.go
+++ modules/user/adapter/pg_repository.go
@@ -17,19 +17,19 @@ func NewUserRepository(db *sql.DB) inbound.UserRepositoryInbound {
 }
 
 func (r *UserRepository) CreateUser(ctx context.Context, input feature.CreateUserRepositoryInput) error {
-	query := `INSERT INTO users (id, name, email, password, enabled_at, created_at, updated_at, version) VALUES ($1, $2, $3, $4, $5, $6, $7)`
+	query := `INSERT INTO users (id, name, email, password, enabled_at, created_at, updated_at, version) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
 
 	result, err := r.Db.ExecContext(
 		ctx,
 		query,
-		input.User.ID,
+		input.User.ID.String(),
 		input.User.Name,
-		input.User.Email,
-		input.User.Password,
+		input.User.Email.String(),
+		input.User.Password.String(),
 		input.User.EnabledAt,
-		input.User.CreatedAt,
-		input.User.UpdatedAt,
-		0, // version
+		input.User.CreatedAt.String(),
+		input.User.UpdatedAt.String(),
+		input.User.Version.String(),
 	)
 	if err != nil {
 		return err
